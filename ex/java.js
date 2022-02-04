71a840ccafe02bfa97beede948667205071f1060
tlswldnek111 <1wo0ow1_jw@naver.com>
2022-02-04 15:09:02 +0900

    sd

---
+++ ex/java.js
@@ -0,0 +1,28 @@
+function changeText(){
+    var h1 = document.getElementsByTagName('h1');
+    h1[0].innerHTML = "취업 화이팅"
+
+}
+function changeText2(){
+    var h1 = document.getElementsByTagName('h1');
+    h1[0].innerHTML = "Hello World"
+}
+function changeText3(){
+    var h3 = document.getElementsByTagName('h3');
+    h3[0].innerHTML = "아저씨"
+}
+function changeText4(){
+    var h3 = document.getElementsByTagName('h3');
+    h3[1].innerHTML = "이제 좀 자리로"
+}
+function changeText5(){
+    var h3 = document.getElementsByTagName('h3');
+    h3[2].innerHTML = "돌아가세요 안 보여요"
+}
+function reset(){
+    var h3 = document.getElementsByTagName('h3');
+    h3[0].innerHTML = "아"
+    h3[1].innerHTML = "이"
+    h3[2].innerHTML = "돌"
+}
+
