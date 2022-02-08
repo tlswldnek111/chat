c7c327adc9795e82e6695c2a3e9bbfbf9d06bfee
tlswldnek111 <1wo0ow1_jw@naver.com>
2022-02-08 16:39:50 +0900

    g2

--- chat.js
+++ chat.js
@@ -1,7 +1,8 @@
-
+const body1 = document.getElementById("bodys2");
+const body2=document.getElementById("bodys");
+const talk = document.querySelector("#talk");
 let play=["놀자","놀고","놀아"];
 let walk=["산책","운동","밖에"];
-
 const intext = document.getElementById('inputTxt');
 var p=document.getElementsByTagName('p');
 function q(){
@@ -42,6 +43,43 @@ function img_ori(){
     intext.value=null;
 
 }
+function bt(){
+    if(document.getElementById('nightb').innerText=="야간모드"){
+        night();
+    }
+    else{
+        light();
+    }
+}
+const btext=document.getElementById("nightb");
+function light(){
+    document.getElementById("catimage").src="./image/morning.png";
+    p[0].innerHTML="낮이다냥~";
+    p[0].style.color="black";
+    intext.value=null;
+    
+    body2.style.backgroundColor="white";
+    body1.style.backgroundColor ="white";
+	btext.style.backgroundColor = "black"
+    btext.style.color="white";
+    btext.innerHTML="야간모드"
+    p[1].style.color="black";
+}
+function night(){
+    body2.style.backgroundColor="black";
+    body1.style.backgroundColor ="black";
+    document.getElementById("catimage").src="./image/night.jpg";
+    p[0].innerHTML="밤이다냥~";
+    p[0].style.color="white";
+    p[1].style.color="white";
+    intext.value=null;
+    
+	btext.style.backgroundColor = "white"
+    btext.style.color="black";
+    btext.innerHTML="라이트모드"
+}
 
 
+
+document.getElementById('nightb').addEventListener('click', bt);
 document.getElementById('submitButton').addEventListener('click', q);
