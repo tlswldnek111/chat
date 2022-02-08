41a37268851cf7db588abf042f481d3627ecefcc
tlswldnek111 <1wo0ow1_jw@naver.com>
2022-02-08 17:25:46 +0900

    g

--- chat.js
+++ chat.js
@@ -5,20 +5,88 @@ let play=["놀자","놀고","놀아"];
 let walk=["산책","운동","밖에"];
 const intext = document.getElementById('inputTxt');
 var p=document.getElementsByTagName('p');
-function q(){
-    var value = document.getElementById("inputTxt").value;
+const btext=document.getElementById("nightb");
+var value = document.getElementById("inputTxt").value;
+var i=0;
+var num=0;
+var json = 
+[
+	{
+		"question" : "안녕",
+		"answer" : "안냥!"
+	},
+	{
+		"question" : "나이?",
+		"answer" : "비밀이다 냥!"
+	},
+	{
+		"question" : "이름?",
+		"answer" : "가바~!"
+	}
+]
 
-for(var i=0;i<play.length;i++){
+var check=0;
+var question="";
+var answer="";
+
+
+function text_check(){
+    if(num==0){//처음 들어옴
+        console.log(num);
+    check=0;
+    for(i=0;i<json.length;i++){
+        if(value == json[i].question){
+            p[0].innerHTML=json[i].answer;
+            num=0;
+            return;
+        }
+    }//json값이 있는경우
+    num+=1;//json값에 없는경우
+    }
+    else if(num==1){
+        p[0].innerHTML= value+" 에 대한 말을 가르쳐 주실래요?(네 or 아니요)";
+        question=value;
+        num+=1;
+    }
+    else if(num==2){//말가르치기
+        if(value=="네"){
+            p[0].innerHTML="대답을 입력해 주세요";
+            num+=1;
+            answer=value;
+            text_push();
+        }
+        else{
+            p[0].innerHTML="뀨";
+            num=0;
+        }
+    }
+}
+function text_push(){
+    if(num==3){
+    json.push({question:`${question}`,answer:`${answer}`});
+    p[0].innerHTML="말을 배웠당!";
+    num=0;
+    }
+}
+
+
+function q(){
+    value = document.getElementById("inputTxt").value;
+for(i=0;i<play.length;i++){
 if(value.includes(play[i])){
     img_play();
+    num=0;
     break;
 }
 else if(value.includes(walk[i])){
     img_walk();
+    num=0;
     break;
 }
-else if(i==play.length-1){
+else if(i==play.length-1){//말배우기
+  
     img_ori();
+    text_check();
     break;
 }
 }
@@ -39,9 +107,6 @@ function img_walk(){
 
 function img_ori(){
     document.getElementById("catimage").src="./image/cat.jpg";
-    p[0].innerHTML="뀨?";
-    intext.value=null;
-
 }
 function bt(){
     if(document.getElementById('nightb').innerText=="야간모드"){
@@ -51,13 +116,12 @@ function bt(){
         light();
     }
 }
-const btext=document.getElementById("nightb");
 function light(){
     document.getElementById("catimage").src="./image/morning.png";
     p[0].innerHTML="낮이다냥~";
     p[0].style.color="black";
     intext.value=null;
-    
+    num=0;
     body2.style.backgroundColor="white";
     body1.style.backgroundColor ="white";
 	btext.style.backgroundColor = "black"
@@ -73,7 +137,7 @@ function night(){
     p[0].style.color="white";
     p[1].style.color="white";
     intext.value=null;
-    
+    num=0;
 	btext.style.backgroundColor = "white"
     btext.style.color="black";
     btext.innerHTML="라이트모드"
